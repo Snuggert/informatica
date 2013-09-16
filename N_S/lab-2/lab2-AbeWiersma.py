8b6615f7dec235fb313b72c083e55f2ee284fa92
Abe <abe.wiersma@hotmail.nl>
2013-09-16 21:11:11 +0200

    changed map name and fixed stuff

--- N_S/lab-2/lab2-AbeWiersma.py
+++ N_S/lab-2/lab2-AbeWiersma.py
@@ -6,7 +6,7 @@
 # File: assignment 1 and 2 for lab2
 # Date: 9-9-2013
 
-import socket, os, sys, argparse, signal, sys, re
+import socket, os, sys, argparse, signal, sys, re, subprocess
 
 responses = {200: 'OK', 404: 'Not Found', 501: 'Not Implemented'}
 
@@ -36,8 +36,12 @@ def build_response_headers(filetype, content):
     else:
         return ''
 
-def return_to_conn(conn, response, location='', content=''):
-    if response == 404:
+def build_response_status(response_proto='HTTP/1.1', response_number=200):
+    response_status = str(response_number) + ' ' + responses[response_number]
+    return '%s %s' % (response_proto, response_status)
+
+def return_to_conn(conn, response_number, location='', content=''):
+    if response_number == 404:
         content = '''
             <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
             <html>
@@ -54,8 +58,7 @@ def return_to_conn(conn, response, location='', content=''):
 
     # response protocol
     response_proto = 'HTTP/1.1'
-    response_status = str(response) + ' ' + responses[response]
-    conn.send('%s %s' % (response_proto, response_status))
+    conn.send(build_response_status(response_proto, response_number))
     conn.send('\n')
 
     filetype = get_file_type(location)
@@ -113,7 +116,28 @@ def serve(port, public_html, cgibin):
         location = request_regex.group(2)
         location_regex = re.search('/cgi-bin(/.*)?', location)
         if location_regex:
-            print('yes, cgi-bin')
+            location_regex = re.search('/cgi-bin(/.*.py)',location)
+            if not location_regex:
+                return_to_conn(conn, 501)
+                continue
+            file_location = cgibin + location_regex.group(1)
+            location_regex = re.search('.*\?(.*)', location)
+            if not location_regex:
+                query_string = ''
+            else:
+                query_string = location_regex.group(1)
+
+            cmd = 'python ' + file_location
+            print(cmd)
+
+            conn.sendall(build_response_status())
+            conn.sendall('\n')
+            # try starting subprocess
+            subprocess.Popen(cmd, shell=True, stdout=conn)
+
+            conn.close()
+            continue
+
         if(location == '/'):
             location = '/index.html'
 
@@ -136,13 +160,10 @@ def serve(port, public_html, cgibin):
             content = content.read()
         except IOError:
             print('File not found')
-
-        # if server can't find requested file in public_html send 404.
-        if not content:
             return_to_conn(conn, 404, location)
 
-        # else return requested file in html format
-        else:
+        # if content return requested file in html format
+        if content:
             return_to_conn(conn, 200, location, content)
             
 
@@ -154,6 +175,6 @@ if __name__ == '__main__':
     p.add_argument('--public_html', help='home directory', default='./public_html')
     p.add_argument('--cgibin', help='cgi-bin directory', default='./cgi-bin')
     args = p.parse_args(sys.argv[1:])
-    public_html = os.path.abspath(args.public_html)
+    public_html = os.path.posixpath(args.public_html)
     cgibin = os.path.abspath(args.cgibin)
     serve(args.port, public_html, cgibin)
