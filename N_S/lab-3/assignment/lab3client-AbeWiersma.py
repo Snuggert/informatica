94fc38345cbd3865a1fd101124538bcf4630c8cb
Snuggert <abe.wiersma@hotmail.nl>
2013-09-21 15:35:04 +0200

    derpeflerp

--- N_S/lab-3/assignment/lab3client-AbeWiersma.py
+++ N_S/lab-3/assignment/lab3client-AbeWiersma.py
@@ -1,31 +1,50 @@
-## Netwerken en Systeembeveiliging Lab 3 - Chat Room (Client)
-## NAME:
-## STUDENT ID:
-from gui import MainWindow
+## Netwerken en Systeembeveiliging Lab 3 - Chat Room (Server)
+## NAME: Abe Wiersma
+## STUDENT ID: 10433120
 
-def loop(port, cert):
+import socket
+
+class Server(object):
+    """ Server object """
+    
+    def __init__(self, port, backlog=5):
+        self.clients = 0
+        # Output socket list
+        self.outputs = []
+
+        # start up the server.
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind(('',port))
+        self.server.listen(backlog)
+
+    def serve():
 	"""
-	GUI loop.
-	port: port to connect to.
+	Chat server entry point.
+	port: The port to listen on.
 	cert: public certificate (bonus task)
+	key: private key (bonus task)
 	"""
-	# The following code explains how to use the GUI.
-	w = MainWindow()
-	# update() returns false when the user quits or presses escape.
-	while w.update():
-		# if the user entered a line getline() returns a string.
-		line = w.getline()
-		if line:
-			w.writeln(line)
+	pass
 
+
+
+class ServerClient(object):
+	""" Client Object"""
+	def __init__(self, name, address):
+		self.name = name
+		self.address = address
+	def get_info():
+		return name + ' at ' + address
 
 
 ## Command line parser.
 if __name__ == '__main__':
 	import sys, argparse
 	p = argparse.ArgumentParser()
-	p.add_argument('--port', help='port to connect to', default=12345, type=int)
+	p.add_argument('--port', help='port to listen on', default=12345, type=int)
 	p.add_argument('--cert', help='server public cert', default='')
+	p.add_argument('--key', help='server private key', default='')
 	args = p.parse_args(sys.argv[1:])
-	loop(args.port, args.cert)
+	serve(args.port, args.cert, args.key)
 
