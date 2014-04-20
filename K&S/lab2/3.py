a0183397453596a41dc9d9212101cf7eb058332c
Snuggert <abe.wiersma@hotmail.nl>
2014-04-20 13:35:49 +0200

    forgot to add py stuff

---
+++ K&S/lab2/3.py
@@ -0,0 +1,38 @@
+import pylab
+import numpy
+from scipy.stats import norm
+import random
+
+
+# function for standard normal equation
+def sne(x):
+    return numpy.power(numpy.e, -0.5 * x * x) / numpy.sqrt(2 * numpy.pi)
+
+
+def main():
+    # 1000 spaced numbers
+    x = numpy.arange(-5.0, 5.0, 0.01)
+    y1 = sne(x)
+
+    pylab.plot(x, y1)
+    pylab.show()
+
+    y = norm.cdf(x)
+    x2 = []
+    for i in range(1000):
+        x2.append(random.normalvariate(0, 1))
+
+    values, base = numpy.histogram(x2, bins=1000)
+    normal_values = values / float(values.sum())
+    #evaluate the cumulative
+    cumulative = numpy.cumsum(normal_values)
+    # plot the cumulative function
+    pylab.plot(base[:-1], cumulative, 'ro')
+
+    pylab.plot(x, y, color="black")
+    pylab.xlabel("Variate")
+    pylab.ylabel("Cumulative Probability")
+    pylab.show()
+
+if __name__ == '__main__':
+    main()
---
+++ K&S/lab2/NaiveBayes.py
@@ -0,0 +1,84 @@
+import re
+import numpy
+import pylab
+
+
+def male_or_female(Mmu, Fmu, Msigma, Fsigma, test_data):
+    Mpost = normpdf(test_data[0], Mmu[0], Msigma[0]) * \
+            normpdf(test_data[1], Mmu[1], Msigma[1]) * \
+            normpdf(test_data[2], Mmu[2], Msigma[2])
+    Fpost = normpdf(test_data[0], Fmu[0], Fsigma[0]) * \
+            normpdf(test_data[1], Fmu[1], Fsigma[1]) * \
+            normpdf(test_data[2], Fmu[2], Fsigma[2])
+
+    if(Mpost > Fpost):
+        return 'M'
+    else:
+        return 'F'
+
+
+def normpdf(x, mu, sigma):
+    u = (x - mu) / abs(sigma)
+    y = (1 / (numpy.sqrt(2 * numpy.pi) * abs(sigma))) * numpy.exp(-u * u / 2)
+    return y
+
+
+def main():
+    ptrn = '^([MF])\s?,([1-9][0-9]*|0),([1-9][0-9]*|0),([1-9][0-9]*.?[0-9]|0)$'
+    lines = [line.strip() for line in open('biometrie2014.csv')]
+    Mdata = []
+    Fdata = []
+    for line in lines:
+        searchResult = re.search(ptrn, line)
+        if searchResult:
+            if searchResult.group(1) == 'M':
+                Mdata.append([float(searchResult.group(2)),
+                              float(searchResult.group(3)),
+                              float(searchResult.group(4))])
+            if searchResult.group(1) == 'F':
+                Fdata.append([float(searchResult.group(2)),
+                              float(searchResult.group(3)),
+                              float(searchResult.group(4))])
+        else:
+            print 'line did not pass regex'
+
+    Mdata = numpy.array(Mdata)
+    Fdata = numpy.array(Fdata)
+
+    Mmu = numpy.median(Mdata, axis=0)
+    Fmu = numpy.median(Fdata, axis=0)
+
+    Msigma = numpy.sqrt(numpy.var(Mdata, axis=0))
+    Fsigma = numpy.sqrt(numpy.var(Fdata, axis=0))
+
+    xWeight = numpy.arange(0, 150, 0.1)
+    Mweight = normpdf(xWeight, Mmu[0], Msigma[0])
+    Fweight = normpdf(xWeight, Fmu[0], Fsigma[0])
+
+    pylab.plot(xWeight, Mweight, color='red')
+    pylab.plot(xWeight, Fweight, color='blue')
+    pylab.legend(['man', 'vrouw'])
+    pylab.show()
+
+    xHeight = numpy.arange(140, 210, 0.1)
+    Mheight = normpdf(xHeight, Mmu[1], Msigma[1])
+    Fheight = normpdf(xHeight, Fmu[1], Fsigma[1])
+
+    pylab.plot(xHeight, Mheight, color='red')
+    pylab.plot(xHeight, Fheight, color='blue')
+    pylab.legend(['man', 'vrouw'])
+    pylab.show()
+
+    xSize = numpy.arange(30, 50, 0.1)
+    Msize = normpdf(xSize, Mmu[2], Msigma[2])
+    Fsize = normpdf(xSize, Fmu[2], Fsigma[2])
+
+    pylab.plot(xSize, Msize, color='red')
+    pylab.plot(xSize, Fsize, color='blue')
+    pylab.legend(['man', 'vrouw'])
+    pylab.show()
+
+    print male_or_female(Mmu, Fmu, Msigma, Fsigma, [60, 183, 42])
+
+if __name__ == '__main__':
+    main()
